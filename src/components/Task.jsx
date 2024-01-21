d9d4fbcfa50ff668b3ba00dffe426884a09917ee
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-21 21:57:59 +0600

    css remove

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,12 @@
-import SearchBox from "./SearchBox";
+// import SearchBox from "./SearchBox";
+import SearchBox from "../ui/SearchBox";
 import Tasker from "../ui/Tasker";
 
 export default function Task() {
   return (
     <>
       <section className="mb-20" id="tasks">
-        <div className="container">
+        <div className="container grid justify-center">
           <SearchBox />
           <Tasker />
         </div>
