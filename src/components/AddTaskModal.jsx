96ae6cd65d2e122ef8c83c96a8c8c03c612bf1a1
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-21 15:14:31 +0600

    Start

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,4 +1,28 @@
-export default function AddTaskModal() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export default function AddTaskModal({ onSave }) {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFav: false,
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "tags") {
+      value = value.split(",");
+    }
+
+    setTask({
+      ...task,
+      [name]: value,
+    });
+  };
   return (
     <>
       <div className="bg-black bg-opacity-70 h-screen w-full z-10 absolute top-0 left-0"></div>
@@ -15,6 +39,8 @@ export default function AddTaskModal() {
               type="text"
               name="title"
               id="title"
+              value={task.title}
+              onChange={handleChange}
               required
             />
           </div>
@@ -25,6 +51,8 @@ export default function AddTaskModal() {
               type="text"
               name="description"
               id="description"
+              value={task.description}
+              onChange={handleChange}
               required
             ></textarea>
           </div>
@@ -36,6 +64,8 @@ export default function AddTaskModal() {
                 type="text"
                 name="tags"
                 id="tags"
+                value={task.tags}
+                onChange={handleChange}
                 required
               />
             </div>
@@ -45,6 +75,8 @@ export default function AddTaskModal() {
                 className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                 name="priority"
                 id="priority"
+                value={task.priority}
+                onChange={handleChange}
                 required
               >
                 <option value="">Select Priority</option>
@@ -59,133 +91,12 @@ export default function AddTaskModal() {
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={() => onSave(task)}
           >
             Create new Task
           </button>
         </div>
       </form>
-
-      {/* <section className="bg-[#1D212B]  font-[Inter] max-md:px-4 lg:text-lg">
-        <nav className="py-6 md:py-8">
-          <div className="container mx-auto flex items-center justify-between gap-x-6">
-            <a href="/">
-              <img
-                className="h-[45px]"
-                src="./assets/lws-logo-en.svg"
-                alt="Lws"
-              />
-            </a>
-
-            <form>
-              <div className="flex">
-                <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
-                  <input
-                    type="search"
-                    id="search-dropdown"
-                    className="z-20 block w-full bg-gray-800 px-4 py-2.5 pr-10 focus:outline-none"
-                    placeholder="Search Task"
-                    required
-                  />
-                  <button
-                    type="submit"
-                    className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-3"
-                  >
-                    <svg
-                      className="h-4 w-4"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                      />
-                    </svg>
-                    <span className="sr-only">Search</span>
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </nav>
-
-        <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
-          <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-            Add New Task
-          </h2>
-
-          
-          <div className="space-y-9 text-white lg:space-y-10">
-      
-            <div className="space-y-2 lg:space-y-3">
-              <label htmlFor="title">Title</label>
-              <input
-                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
-                type="text"
-                name="title"
-                id="title"
-                required
-              />
-            </div>
-            <div className="space-y-2 lg:space-y-3">
-              <label htmlFor="description">Description</label>
-              <textarea
-                className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
-                type="text"
-                name="description"
-                id="description"
-                required
-              ></textarea>
-            </div>
-            <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
-              <div className="space-y-2 lg:space-y-3">
-                <label htmlFor="tags">Tags</label>
-                <input
-                  className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
-                  type="text"
-                  name="tags"
-                  id="tags"
-                  required
-                />
-              </div>
-              <div className="space-y-2 lg:space-y-3">
-                <label htmlFor="priority">Priority</label>
-                <select
-                  className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
-                  name="priority"
-                  id="priority"
-                  required
-                >
-                  <option value="">Select Priority</option>
-                  <option value="low">Low</option>
-                  <option value="medium">Medium</option>
-                  <option value="high">High</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <div className="mt-16 flex justify-center lg:mt-20">
-            <button
-              type="submit"
-              className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-            >
-              Create new Task
-            </button>
-          </div>
-        </form>
-
-        <footer className="py-6 md:py-8">
-          <div className="container mx-auto">
-            <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-              Copyright ©2023 | All rights reserved by Learn with Sumit
-            </p>
-          </div>
-        </footer> 
-      </section> */}
     </>
   );
 }
